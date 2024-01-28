fce706a6e4b4f525467a3ed637cff8f60a9e9a7d
Ataberk Sevinçli <sevincli.ataberk@hotmail.com>
2024-01-28 21:31:20 +0300

    rentacar yapıldı

---
+++ Patika-RentACar/src/App.java
@@ -0,0 +1,13 @@
+import core.Helper;
+import view.AdminView;
+import view.LoginView;
+import business.UserManager;
+
+public class App {
+    public static void main(String[] args) {
+        //Connection conn = Db.getInstance();
+        Helper.setTheme();
+        LoginView loginView = new LoginView();
+
+    }
+}
